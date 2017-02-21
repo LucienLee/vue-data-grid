ad132ae6d5aae461b099f4e76438ad896bb43979
Lucien Lee <lkiral7903@gmail.com>
2017-02-21 17:44:43 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  #app
+    hello
+</template>
+
+<script>
+import Hello from './components/Hello'
+
+export default {
+  name: 'app',
+  components: {
+    Hello
+  }
+}
+</script>
+
+<style lang="sass">
+@import "sass/_variable.sass"
+
+body
+  background: $bg-color
+#app
+  font-family: 'Avenir', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App'
+
+import 'normalize.css'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
